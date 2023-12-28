a05149cc96da570f13fd1c91737bd16cfbb88a36
Allson <100061407+Allson-TA@users.noreply.github.com>
2023-12-28 20:37:25 +0800

    java

---
+++ src/dey01/ArrayDemo1.java
@@ -0,0 +1,13 @@
+package dey01;
+
+public class ArrayDemo1 {
+
+	public static void main(String[] args) {
+		
+		String[] cars = {"Honda","Toyota","BMW","Benz","BYD","Tesla"};
+		System.out.println(cars);
+		System.out.println(cars);
+
+	}
+
+}
