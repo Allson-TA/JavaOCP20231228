32bea3de7c33da9a9b1036aad1d3402c740b1658
Allson <100061407+Allson-TA@users.noreply.github.com>
2023-12-28 20:40:04 +0800

    java

--- src/dey01/ArrayDemo1.java
+++ src/dey01/ArrayDemo1.java
@@ -1,4 +1,4 @@
-package dey01;
+package dey01;import java.util.Arrays;
 
 public class ArrayDemo1 {
 
@@ -6,8 +6,22 @@ public class ArrayDemo1 {
 		
 		String[] cars = {"Honda","Toyota","BMW","Benz","BYD","Tesla"};
 		System.out.println(cars);
-		System.out.println(cars);
-
+		System.out.println(cars[0]);
+		System.out.println(cars[1]);
+		System.out.println(cars[2]);
+		System.out.println(cars[3]);
+		System.out.println(cars[4]);
+		System.out.println(cars[5]);
+		
+		for(int i=0;i<cars.length;i++) {
+			System.out.println(cars[i]);
+		}
+		
+		for(String car:cars) {
+			System.out.println(car);
+		}
+		
+		System.out.println(Arrays.toString(cars));
 	}
 
 }
